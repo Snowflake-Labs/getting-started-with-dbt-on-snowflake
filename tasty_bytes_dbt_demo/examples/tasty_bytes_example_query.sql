bc961f17d147c4c964d40d8952b120adde0baf50
getting-started-with-dbt-on-snowflake <hchung@eulidia.com>
2026-04-01 08:39:57 +0000

    experimentation librairie et tags snowflake

--- tasty_bytes_dbt_demo/examples/tasty_bytes_example_query.sql
+++ tasty_bytes_dbt_demo/examples/tasty_bytes_example_query.sql
@@ -20,3 +20,34 @@ JOIN tb_101.raw.order_header oh
 ON cl.customer_id = oh.customer_id
 GROUP BY cl.customer_id, cl.city, cl.country, cl.first_name,
 cl.last_name, cl.phone_number, cl.e_mail;
+
+
+
+
+-- Create NETWORK RULE for external access integration
+
+CREATE OR REPLACE NETWORK RULE dbt_network_rule
+  MODE = EGRESS
+  TYPE = HOST_PORT
+  -- Minimal URL allowlist that is required for dbt deps
+  VALUE_LIST = (
+    'hub.getdbt.com',
+    'codeload.github.com'
+    );
+
+-- Create EXTERNAL ACCESS INTEGRATION for dbt access to external dbt package locations
+
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
+  ALLOWED_NETWORK_RULES = (dbt_network_rule)
+  ENABLED = TRUE;
+
+GRANT USAGE ON DATABASE TASTY_BYTES_DBT_DB TO ROLE DEMO_ROLE;
+
+-- Elementary needs to create its own schema (usually named 'ELEMENTARY')
+GRANT CREATE SCHEMA ON DATABASE TASTY_BYTES_DBT_DB TO ROLE DEMO_ROLE;
+
+
+CREATE TAG TASTY_BYTES_DBT_DB.dev.cost_center;
+
+-- 2. (Optional) Let your dbt role use this tag
+GRANT APPLY ON TAG TASTY_BYTES_DBT_DB.dev.cost_center TO ROLE demo_role;
